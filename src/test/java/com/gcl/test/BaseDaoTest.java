d164eb75d5e4f2081352756edc2762de87826635
ngkjcxm <17865923028@163.com>
2016-10-18 21:42:32 +0800

    项目框架修正

--- src/test/java/com/gcl/test/BaseDaoTest.java
+++ src/test/java/com/gcl/test/BaseDaoTest.java
@@ -1,11 +1,9 @@
 package com.gcl.test;
 
-import com.sdut.ngxykjc.bean.Dog;
+import com.sdut.ngxykjc.bean.HorizontalProject;
 import com.sdut.ngxykjc.dao.BaseDao;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.List;
 
 /**
  * Created by 郭昌仑 on 2016/10/18.
@@ -16,42 +14,16 @@ import java.util.List;
 public class BaseDaoTest extends BaseJunitTest {
 
     @Autowired
-    private BaseDao<Dog> baseDao;
-
-    public void testInsert(){
-        Dog dog = new Dog();
-        dog.setAge(1);
-        dog.setName("Toni");
-        baseDao.save(dog);
-    }
-
-    public void testGet(){
-        Dog dog = baseDao.get(Dog.class, 1);
-        System.out.println(dog);
-    }
+    private BaseDao<HorizontalProject> baseDao;
 
     @Test
-    public void testGetWithSql(){
-        List<Dog> dogs = baseDao.get(Dog.class, "SELECT * FROM dog");
-        System.out.println(dogs);
-    }
+    public void testHor(){
+        HorizontalProject pro = new HorizontalProject();
+        pro.setAuditor("张三");
+        pro.setConsignor("李四");
+        pro.setDirectorUnit("计算机");
 
-    public void testUpdate(){
-        Dog dog = new Dog();
-        dog.setId(1);
-        dog.setName("update");
-        dog.setAge(100);
-
-        baseDao.update(dog);
-    }
-
-    public void testDelete(){
-        Dog dog = new Dog();
-        dog.setId(1);
-        dog.setName("update");
-        dog.setAge(100);
-
-        baseDao.delete(dog);
+        baseDao.save(pro);
     }
 
 }
